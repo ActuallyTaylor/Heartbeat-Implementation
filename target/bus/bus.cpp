1ee22444d7105ae8a6c8072504feaece3f3d9c6c
Taylor Lineman <git@actuallytaylor.com>
2026-09-20 14:13:41 -0400

    Added http and json libraries
    Signed-off-by: Taylor Lineman <git@actuallytaylor.com>

--- target/bus/bus.cpp
+++ target/bus/bus.cpp
@@ -3,9 +3,19 @@
  * until they are read by another device.
  */
 
-#include <iostream>
+#include "httplib.hpp"
+#include "json.hpp"
 
 int main() {
+    httplib::Server svr;
+
+    svr.Post("/send", [&](const auto& req, auto& res) {
+        auto json = nlohmann::json::parse(res.body);
+        auto id = json["device_id"].get<int>();
+
+    });
+
+    svr.listen("0.0.0.0", 8080);
 
     return 0;
 }
